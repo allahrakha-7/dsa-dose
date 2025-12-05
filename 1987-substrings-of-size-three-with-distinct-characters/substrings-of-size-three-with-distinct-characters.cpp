a4da8a2a5b587f0ef29fc90d690b5652c5d323a4
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2025-12-05 18:31:50 +0500

    Time: 0 ms (100.00%) | Memory: 8.8 MB (25.87%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -2,40 +2,40 @@ class Solution {
 public:
     int countGoodSubstrings(string s) {
         // base case
-        if (s.size() < 3) {
-            
-            return 0;
-        }
+        if (s.size() < 3) return 0;
 
         int length = 0;
         
         // hashmap to store characters
-        unordered_map<char, int> Freq;
+        unordered_map<char, int> freq_count;
 
         for (int i = 0; i < 3; i++) {
 
-            Freq[s[i]]++;
+            freq_count[s[i]]++;
+
         }
 
-        if (Freq.size() == 3) {
+        if (freq_count.size() == 3) {
 
             length++;
+
         }
 
         for (int i = 3; i < s.size(); i++) {
 
-            Freq[s[i - 3]]--;
+            freq_count[s[i - 3]]--;
 
-            if (Freq[s[i - 3]] == 0) {
+            if (freq_count[s[i - 3]] == 0) {
 
-                Freq.erase(s[i - 3]);
+                freq_count.erase(s[i - 3]);
             }
 
-            Freq[s[i]]++;
+            freq_count[s[i]]++;
 
-            if (Freq.size() == 3) {
+            if (freq_count.size() == 3) {
 
                 length++;
+                
             }
         }
         return length;
