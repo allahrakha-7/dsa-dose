5fe59c82ac0874c642c3ec9f7934d59b7028f8eb
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2025-12-25 02:02:06 +0500

    Time: 10 ms (43.35%) | Memory: 7.9 MB (36.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int fib(int n) {
 
-        // base case
-        if (n == 0) return 0;
+        // base cases
+        if (n == 0) return 0; 
         if (n == 1) return 1;
 
-        // fabonacci formula
+        // fibonacci formula
         return fib(n - 1) + fib(n - 2);
     }
 };
